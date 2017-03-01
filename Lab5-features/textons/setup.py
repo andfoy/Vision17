6abc2fce9c1a58cc0afce877b5173dc1da6a4f20
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-02-28 21:54:12 -0500

    Cython setup file update

--- Lab5-features/textons/setup.py
+++ Lab5-features/textons/setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import numpy as np
 
 setup(
+    include_dirs=[np.get_include()],
     ext_modules=cythonize([
         Extension("lib_textons", ["lib_textons.pyx"])
     ])
