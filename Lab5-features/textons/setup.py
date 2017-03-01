00142ebbc9ad7ab3cf121bafb49a5d715f07438e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-01 15:31:10 -0500

    Setup.py update

--- Lab5-features/textons/setup.py
+++ Lab5-features/textons/setup.py
@@ -6,8 +6,8 @@ import numpy as np
 setup(
     include_dirs=[np.get_include()],
     ext_modules=cythonize([
-        Extension('textons.kmc2', sources=[
+        Extension('kmc2', sources=[
                   'kmc2.c'], extra_compile_args=['-O3']),
-        Extension("textons.lib_textons", ["lib_textons.pyx"])
+        Extension("lib_textons", ["lib_textons.pyx"])
     ]),
 )
