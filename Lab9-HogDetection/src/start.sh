4778c68b19793b1c1bd03bf77111597dc4d9bcf8
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-13 15:35:45 -0500

    HOG: Source ground truth labels

--- Lab9-HogDetection/src/start.sh
+++ Lab9-HogDetection/src/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 DATASET_URL="http://157.253.63.7/lab9Detection.tar"
+LABELS_URL="http://mmlab.ie.cuhk.edu.hk/projects/WIDERFace/support/bbx_annotation/wider_face_split.zip"
+LABELS_FILE="wider_face_split.zip"
 DATASET_ROOT="lab9Detection"
 DATASET_FILE="lab9Detection.tar"
 DATA_PATH="data"
@@ -21,6 +23,11 @@ if [ ! -d $DATA_PATH ]; then
         rm $ZIP_FILE
     done
     printf "Done!\n"
+
+    printf "nDownloading labels...\n"
+    wget -c $LABELS_URL
+    unzip $LABELS_FILE
     cd ..
+    # python process_labels.py
 fi
 
