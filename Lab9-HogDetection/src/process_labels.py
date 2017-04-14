3d1ad3e9fb7c1cc0d9e43de67037f55bf27b10ba
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-13 20:08:53 -0500

    HOG: Python preprocessing script update

--- Lab9-HogDetection/src/process_labels.py
+++ Lab9-HogDetection/src/process_labels.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     file = os.path.join(LABELS_ROOT, 'wider_face_train.mat')
     out_file = os.path.join(LABELS_ROOT, 'face_train.npz')
     values, _ = load_matlab(file)
-    if not isinstance(list, values['file_list']):
+    if not isinstance(values['file_list'], list):
         values['file_list'] = get_matlab_value(values['file_list'][0])
-    if not isinstance(list, values['face_bbx_list']):
+    if not isinstance(values['face_bbx_list'], list):
         values['face_bbx_list'] = get_matlab_value(values['face_bbx_list'][0])
     bounding_boxes = {}
     for file_group, bbx_group in zip(values['file_list'],
