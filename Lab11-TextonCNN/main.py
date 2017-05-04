a1a85ea1c600409908cd6f80c38305da179545cd
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-03 19:09:35 -0500

    CNN: Debugging model dimensions

--- Lab11-TextonCNN/main.py
+++ Lab11-TextonCNN/main.py
@@ -84,12 +84,18 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(50, num_classes)
 
     def forward(self, x):
+        print("In: {0}".format(x.size()))
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        print("CONV1: {0}".format(x.size()))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        print("CONV2: {0}".format(x.size()))
         x = x.view(-1, 320)
+        print("Reshape: {0}".format(x.size()))
         x = F.relu(self.fc1(x))
+        print("FC1: {0}".format(x.size()))
         x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
+        print("FC2: {0}".format(x.size()))
         return F.log_softmax(x)
 
 
@@ -108,7 +114,6 @@ def train(epoch):
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output = model(data)
-        print(output.size())
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
