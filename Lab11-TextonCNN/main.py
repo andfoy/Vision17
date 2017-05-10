c642dbd5b50dc6704d07b8e9993b43b33b9abcd8
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-09 19:47:14 -0500

    CNN: Added embed representation for visualization

--- Lab11-TextonCNN/main.py
+++ Lab11-TextonCNN/main.py
@@ -94,6 +94,28 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(512 * 7 * 7, 50)
         self.fc2 = nn.Linear(50, num_classes)
 
+    def embed(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = F.relu(self.conv7(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv8(x))
+        x = F.relu(self.conv9(x))
+
+        # print("CONV5 {0}".format(x.size()))
+        x = x.view(-1, x.size(1) * x.size(2) * x.size(3))
+        # x = F.dropout()
+        # x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return x
+
     def forward(self, x):
         # print("In: {0}".format(x.size()))
         x = F.relu(self.conv1(x))
